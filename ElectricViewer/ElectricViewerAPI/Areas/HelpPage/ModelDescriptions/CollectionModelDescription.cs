c184d8274fa0ffb3ad063fe5fc260c7182a88a6f
Kael's Surface Pro <kaelkong@me.com>
2017-03-14 09:19:42 +0800

    创建接口项目

---
+++ ElectricViewer/ElectricViewerAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ElectricViewerAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ElectricViewer/ElectricViewerAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ElectricViewerAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
